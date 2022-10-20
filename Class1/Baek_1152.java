46cd62fe8c53347f8dd9e6c9750b65d5cd39e908
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-10-20 19:25:21 +0900

    백준 1152 단어의 개수

--- Class1/Baek_1152.java
+++ Class1/Baek_1152.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 public class Baek_1152 {
         public static void main(String[] args) throws IOException {
+
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String s = br.readLine().trim();
 
