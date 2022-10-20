63c3d3e65ad3af236c28a658c283fb32068b0cdf
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-10-20 19:47:15 +0900

    백준 2920 음계

--- Class1/baek_2920.java
+++ Class1/baek_2920.java
@@ -1,9 +1,25 @@
 package Class1;
 
+//2920 음계
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class baek_2920 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String result;
+        String s = br.readLine();
+        switch (s){
+            case "1 2 3 4 5 6 7 8":
+                System.out.println("ascending");
+                break;
+            case "8 7 6 5 4 3 2 1":
+                System.out.println("descending");
+                break;
+            default: //같은 값이 없으면
+                System.out.println("mixed");
+        }
     }
-
 }
 
