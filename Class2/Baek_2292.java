5a64d5ff071a1eebdc056a52e8dbfade50333ad7
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-10-29 22:16:40 +0900

    백준2292 벌집

---
+++ Class2/Baek_2292.java
@@ -0,0 +1,30 @@
+package Class2;
+
+import java.util.Scanner;
+
+//2292 벌집
+public class Baek_2292 {
+    public static void main(String[] args) {
+    /*
+    규칙
+    1- 1개방 지남 (1)
+    2-7 2개방 지남 (+6)
+    8-19 3개방 지남 (+12)
+    20-37 4개방 지남 (+18)
+    38-61 5개방 지남 (+24)
+    */
+        Scanner in = new Scanner(System.in); //scanner 객체 생성
+        int N = in.nextInt();
+        int passRoomCount = 1;
+        int range = 2;
+        if (N == 1) {
+            System.out.println(1);
+        } else {
+            while (range <= N) {
+                range = range + (6 * passRoomCount);
+                passRoomCount++;
+            }
+            System.out.print(passRoomCount);
+        }
+    }
+}
