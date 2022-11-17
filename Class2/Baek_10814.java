3bd40f23d5787b0f7cf2738ec762b5a2e2cada3c
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-11-17 23:29:41 +0900

    백준 10814

---
+++ Class2/Baek_10814.java
@@ -0,0 +1,41 @@
+package Class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek_10814 {
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int N = Integer.parseInt(br.readLine());
+
+            // 입력되는 나이의 범위 : 1 ~ 200
+            //stringBuilder 타입이 요소로 들어가는 길이 201의 배열
+            StringBuilder[] p = new StringBuilder[201];
+
+            //객체배열의 인덱스에 각 StringBuilder 객체를 생성해준다.
+            for(int i = 0; i < p.length; i++) {
+                p[i] = new StringBuilder();
+            }
+
+            for(int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                int age = Integer.parseInt(st.nextToken());
+                String name = st.nextToken();
+                // 카운팅 정렬 : 나이를 index 로 하여 해당 배열에 나이와 이름을 append() 한다
+                //p[21] = 21 김채은 21 김코딩 (같은 나이가 2개 있을 시 들어온 순서대로 입력됨)
+                p[age].append(age).append(' ').append(name).append('\n');
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(StringBuilder val : p) { //p를 돌며 Stringbuilder타입의 val에 입력받아 더해줌 (빈 객체일시 빈 객체가 더해지므로 무효)
+                sb.append(val);
+            }
+
+            System.out.println(sb);
+        }
+    }
+
--- Class2/Mick.java
+++
@@ -1,4 +0,0 @@
-package Class2;
-
-public class Mick {
-}
