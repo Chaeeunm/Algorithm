358f4b4ad495a63c951c0e8af04c1781fb574df6
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-11-19 16:41:59 +0900

    백준 수정

--- Class2/Baek_10814.java
+++ Class2/Baek_10814.java
@@ -14,27 +14,26 @@ public class Baek_10814 {
 
             // 입력되는 나이의 범위 : 1 ~ 200
             //stringBuilder 타입이 요소로 들어가는 길이 201의 배열
-            StringBuilder[] p = new StringBuilder[201];
+            StringBuilder[] sort = new StringBuilder[201];
 
             //객체배열의 인덱스에 각 StringBuilder 객체를 생성해준다.
-            for(int i = 0; i < p.length; i++) {
-                p[i] = new StringBuilder();
+            for(int i = 0; i < sort.length; i++) {
+                sort[i] = new StringBuilder();
             }
 
             for(int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
                 int age = Integer.parseInt(st.nextToken());
                 String name = st.nextToken();
-                // 카운팅 정렬 : 나이를 index 로 하여 해당 배열에 나이와 이름을 append() 한다
-                //p[21] = 21 김채은 21 김코딩 (같은 나이가 2개 있을 시 들어온 순서대로 입력됨)
-                p[age].append(age).append(' ').append(name).append('\n');
+                // 카운팅 정렬 : 나이를 index 로 하여 해당 배열에 나이와 이름을 더한다
+                //p[21] = 21 김채은 '\n' 21 김코딩 '\n' (같은 나이가 2개 있을 시 들어온 순서대로 입력됨)
+                sort[age].append(age).append(' ').append(name).append('\n');
             }
 
             StringBuilder sb = new StringBuilder();
-            for(StringBuilder val : p) { //p를 돌며 Stringbuilder타입의 val에 입력받아 더해줌 (빈 객체일시 빈 객체가 더해지므로 무효)
+            for(StringBuilder val : sort) { //p를 돌며 Stringbuilder 타입의 val에 입력받아 더해줌 (빈 객체일시 빈 객체가 더해지므로 무효)
                 sb.append(val);
             }
-
             System.out.println(sb);
         }
     }
--- Class2/Baek_11650.java
+++ Class2/Baek_11650.java
@@ -2,15 +2,11 @@ package Class2;
 /*2차원 평면 위의 점 N개가 주어진다.
 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음
 출력하는 프로그램을 작성하시오.*/
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Baek_11650 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
@@ -21,11 +17,11 @@ public class Baek_11650 {
             arr[i][1] = sc.nextInt(); //y좌표값 입력
         }
 
-        Arrays.sort(arr,(a, b)->{ //arr배열을 정력, a,b를 넣었을 때 만약 a와 b의 x좌표가 같다면
+        Arrays.sort(arr,(a, b)->{ //arr배열을 정렬, a,b를 넣었을 때 만약 a와 b의 x좌표가 같다면
             if(a[0] == b[0]) {
-                return a[1] - b[1]; //y좌표값 비교?
+                return a[1] - b[1]; //y 좌표값 비교? (오른쪽이 크면 음수/작으면 양수 반환)
             }else {
-                return a[0] - b[0]; //그렇지 않다면
+                return a[0] - b[0]; //그렇지 않다면 x좌표 값 비교
             }
         });
 
