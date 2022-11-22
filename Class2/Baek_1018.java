ce043568bff09d1b98de6d272da76c48df302d93
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-11-22 23:54:53 +0900

    백준 1018 수정

--- Class2/Baek_1018.java
+++ Class2/Baek_1018.java
@@ -1,5 +1,20 @@
 package Class2;
-
+//갈수 있는데까지 가보는 브루투.......
 public class Baek_1018 {
+    /*일단 2차 배열로 입력받음
+    for(int row =0; row<M-9; row++){가로줄 한칸씩 넘어가며
+    for(int i=0; i<N-9; i++){세로줄도 한칸씩 넘어가며
+      for(int j=i; j<=8; j++){작은 배열의 8크기까지 확인
+          만약 arr[row][j]=B 이면
+              j+ 홀수 = W가 아니면 count++
+              j+짝수 = B가 아니면 count++
+          만약 arr[row][j]=W이면
+            j+ 홀수 = B가 아니면 count++
+            j+짝수 = W가 아니면 count++
+
+
+
+     */
+
 
 }
