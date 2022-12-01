ca54c58081e645dd0bd6da997e9b15f099e80191
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-12-01 20:46:31 +0900

    백준 9012 수정

--- Class2/Baek_9012.java
+++ Class2/Baek_9012.java
@@ -1,7 +1,27 @@
 package Class2;
 
-public class Baek_9012 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class Baek_9012 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            String ps = br.readLine();
+            for (int j = 0; j < ps.length(); j++) {
+                if (ps.charAt(j) == '(') count++;
+                else if (ps.charAt(j) == ')') count--;
+            }
+            if (count == 0) sb.append("YES").append('\n');
+            else sb.append("NO").append('\n');
+        }
+        System.out.println(sb);
     }
 }
