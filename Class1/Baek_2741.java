2cad12db74c7ceeeedc8b2f715e8b40f3671d20f
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-12-09 23:54:32 +0900

    백준 2741

---
+++ Class1/Baek_2741.java
@@ -0,0 +1,14 @@
+package Class1;
+
+import java.util.*;
+
+public class Baek_2741 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        for (int i=1; i<=N; i++){
+            System.out.println(i);
+        }
+    }
+}
