1261ae41b9a2b07010041ca0480565ce9b56a976
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2022-12-14 23:55:20 +0900

    백준 2475

---
+++ Class1/Baek_2475.java
@@ -0,0 +1,17 @@
+package Class1;
+import java.util.*;
+public class Baek_2475 {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            int d = in.nextInt();
+            int e = in.nextInt();
+
+            int num = (a*a + b*b + c*c + d*d + e*e)%10;
+            System.out.println(num);
+        }
+    }
+}
