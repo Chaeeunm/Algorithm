e09cb495c6e21925cfc24e35cc19630ad1119406
Chaeeunm <111339014+Chaeeunm@users.noreply.github.com>
2023-02-03 17:08:26 +0900

    Day41

---
+++ Coplit/DailyCoding/Day41.java
@@ -0,0 +1,23 @@
+package Coplit.DailyCoding;
+
+public class Day41 {
+    public int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int middle = (right + left) / 2;
+
+            if (arr[middle] == target) return middle;
+
+            if (target < arr[middle]) {
+                right = middle - 1;
+
+            } else {
+                left = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
