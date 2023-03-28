cb29e719d8e219113412fdfbcacf64fd0d69f248
Chaeeunm <rlacodms0525@naver.com>
2023-03-28 18:11:42 +0900

    3월 3주차 복습

--- Class2/Baek_9012.java
+++ Class2/Baek_9012.java
@@ -3,8 +3,6 @@ package Class2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
 
 public class Baek_9012 {
     public static void main(String[] args) throws IOException {
@@ -12,17 +10,23 @@ public class Baek_9012 {
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
-        //괄호 두개를 괄호 하나로 합치는 방법도 있음!!!!!!
+        //괄호 두개를 괄호 하나로 합치는 방법
         for (int i = 0; i < N; i++) {
-            int count = 0;
+            //int count =0;
             String ps = br.readLine();
-            for (int j = 0; j < ps.length(); j++) {
-                if (ps.charAt(j) == '(') count++;
-                else if (ps.charAt(j) == ')') count--;
-                if(count<0) break; //반복문 벗어나고 count != 0이기에 NO 출력
-            }
-            if (count == 0) sb.append("YES").append('\n');
+            while(ps.contains("()")){
+                ps.replace("()","");
+                }
+            if(ps.length()==0) sb.append("YES").append('\n');
             else sb.append("NO").append('\n');
+
+//            for (int j = 0; j < ps.length(); j++) {
+//                if (ps.charAt(j) == '(') count++;
+//                else if (ps.charAt(j) == ')') count--;
+//                if(count<0) break; //반복문 벗어나고 count != 0이기에 NO 출력
+//            }
+//            if (count == 0) sb.append("YES").append('\n');
+//            else sb.append("NO").append('\n');
         }
         System.out.println(sb);
     }
