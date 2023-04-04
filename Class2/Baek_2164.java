8bdcfce8a491626398a869cd5e02653dd333c56c
Chaeeunm <rlacodms0525@naver.com>
2023-04-04 14:52:03 +0900

    3월 4주차

--- Class2/Baek_2164.java
+++ Class2/Baek_2164.java
@@ -1,28 +1,23 @@
 package Class2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Baek_2164 {
-    //제일 위에 있는 카드(1)를 바닥에 버린다.
-    //제일 위에 있는 카드를 젱ㄹ 아래에 있는 카드(N) 밑으로 옮긴다.
-    //N이 주어졌을 때 제일 마지막에 남게 되는 카드를 구하자
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        Queue<Integer> q = new LinkedList<>(); //Queue 생성
-
-        for (int i = 1; i <= N; i++) { // 큐에 N까지 채워넣음 1 2 3 4 5
-            q.add(i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N  = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList();
+        for (int i =1; i<=N;i++){
+            q.offer(i);
         }
-
-        while (q.size() > 1) {
-            q.poll(); //젤 위의 카드 버림
-            q.add(q.poll()); //젤 위의 카드 젤 밑으로 옮김
+        while (q.size()>1){
+            q.remove();
+            q.offer(q.poll());
         }
         System.out.println(q.poll());
-
     }
 }
--- Class2/Baek_2751.java
+++ Class2/Baek_2751.java
@@ -2,40 +2,35 @@ package Class2;
 //수 정렬하기
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-
-/*Arrays.sort() 의 경우 dual-pivot Quicksort 알고리즘을 사용.
- 평균 시간복잡도가 O(nlogn) 이고 매우 빠른 알고리즘인 것은 맞으나 최악의 경우 시간복잡도는 O(n2)임!!
- => 최악의 경우에도 O(nlogn) 을 보장하거나 혹은, O(n) 에 가까운 정렬 알고리즘을 사용해야 한다
-
-<2가지 방법>
-
-1.Collections.sort()  => 합병 및 삽입정렬 알고리즘을 사용
-    합병정렬(Merge Sort)의 경우 최선, 최악 모두 O(nlogn)  을 보장하고. 삽입정렬(Insertion sort)의 경우 최선의 경우는 O(n) , 최악의 경우는 O(n2) 이다
-    대신에 Collections.sort()를 사용하고자 한다면 일반적인 primitive 배열이 아닌 List 계열(ArrayList, LinkedList 등..)의 자료구조를 사용하여 정렬해야한다.
-
-
-2.Counting sort (카운팅 정렬)을 응용
-
-*/
 public class Baek_2751 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-//ArrayList String타입으로 하면 에러남
-        ArrayList<Integer> list = new ArrayList<>();
+
+        boolean count[] = new boolean[2000001];
+
         int N = Integer.parseInt(br.readLine());
-        for(int i=0; i<N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+
+        for(int i=0; i<N ; i++){
+            count[Integer.parseInt(br.readLine())+1000000] = true;
         }
-
-        Collections.sort(list); //Array.sort보다 시간복잡도가 빠른 Collection.sort를 사용하여 정렬
-
-        for (int s:list) {
-            //append(s+'\n')으로 하면 에러남 왜?
-            sb.append(s).append('\n');
+        for(int i=0;i<2000001; i++){
+            if(count[i])sb.append(i-1000000).append('\n');
         }
         System.out.println(sb);
+//
+//        ArrayList<Integer> list = new ArrayList<>();
+//        int N = Integer.parseInt(br.readLine());
+//        for(int i=0; i<N; i++) {
+//            list.add(Integer.parseInt(br.readLine()));
+//        }
+//
+//        Collections.sort(list); //Array.sort보다 시간복잡도가 빠른 Collection.sort를 사용하여 정렬
+//
+//        for (int s:list) {
+//            sb.append(s).append('\n');
+//        }
+//        System.out.println(sb);
     }
+
 }
