5036de99203b65775591711a19138e22057b705b
Chaeeunm <rlacodms0525@naver.com>
2023-04-06 15:10:39 +0900

    4월 2주차

---
+++ Class3/Baek_1003.java
@@ -0,0 +1,4 @@
+package Class3;
+
+public class Baek_1003 {
+}
--- Class3/Baek_11723.java
+++ Class3/Baek_11723.java
@@ -1,18 +1,15 @@
 package Class3;
 
-//all: S를 {1, 2, ..., 20} 으로 바꾼다.
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.StringTokenizer;
-
 //중복이 안된다
 //순서는 상관이 없는듯?
 //중복을 허용하지 않으므로 Set으로 구현
 public class Baek_11723 {
-    static HashSet S = new HashSet(20);
+    static HashSet<Integer> S = new HashSet(20);
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,30 +20,40 @@ public class Baek_11723 {
             StringTokenizer st = new StringTokenizer(br.readLine()," ");
             switch (st.nextToken()){
                 case "add":
-                    S.add(st.nextToken());
+                    S.add(Integer.parseInt(st.nextToken()));
+                    break;
                 case "remove":
-                    S.remove(st.nextToken());
+                    S.remove(Integer.parseInt(st.nextToken()));
+                    break;
                 case "check":
-                    sb.append(check(st.nextToken())).append('\n');
+                    sb.append(check(Integer.parseInt(st.nextToken()))).append("\n");
+                    break;
                 case "toggle":
-                    toggle(st.nextToken());
+                    toggle(Integer.parseInt(st.nextToken()));
+                    break;
                 case "all":
                     all();
+                    break;
                 case "empty":
                     S.clear();
+                    break;
             }
-
         }
+        System.out.println(sb);
+
     }
-    public static String check(String a){
-        return (S.contains(a))?"1":"0";
+    public static String check(int num){
+        return (S.contains(num))?"1":"0";
     }
-    public static void toggle(String a){
-        if(S.contains(a))S.remove(a); //있으면 제거
-        else S.add(a); //없으면 추가
+    public static void toggle(int num){
+        if(S.contains(num))S.remove(num); //있으면 제거
+        else S.add(num); //없으면 추가
     }
     public static void all() {
         for(int i=1;i<21;i++)
         S.add(i);
     }
 }
+
+//객체로 해서 String으로 받았을 때랑 Integer로 받았을 때랑 값이 달라짐 왜일까?
+
