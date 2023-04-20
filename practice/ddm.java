5f77349cbbc08de9ec16415ce6781ff35adf9bd9
Chaeeunm <rlacodms0525@naver.com>
2023-04-20 14:56:48 +0900

    기초연습

--- practice/ddm.java
+++ practice/ddm.java
@@ -1,5 +1,33 @@
 package practice;
 
-public class ddm {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ddm {
+    public static void main(String[] args) throws IOException {
+
+        String[][] strs = new String[5][5];
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 0; i<5; i++){
+            strs[i] = br.readLine().split(" ");
+        }
+        String result = ppm(strs);
+        System.out.println(result);
+
+    }
+    public static String ppm(String[][] strs) {
+        for (int i = 0; i < 5; i++) {
+            int num = Math.abs(Integer.parseInt(strs[i][i]));
+            int rem = 0;
+            for (int j = 0; j < 5; j++) {
+                rem += Math.abs(Integer.parseInt(strs[i][j]));
+            }
+            rem -= num;
+            if (num < rem) return "no";
+        }
+        return "yes";
+    }
 }
---
+++ practice/offset.java
@@ -0,0 +1,50 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class offset {
+    public static void main(String[] args) throws IOException {
+        String[][] strs = new String[5][5];
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<5; i++){
+            strs[i] = br.readLine().split(" ");
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                sb.append(offset(i,j,strs)).append(" ");
+            }
+            sb.append('\n');
+            }
+        System.out.println(sb);
+    }
+    static String offset(int i,int j, String[][] strs) {
+                int num = Integer.parseInt(strs[i][j]);
+                switch (i) {
+                    case 0:
+                        if (num < Integer.parseInt(strs[i + 1][j])) offsetJ(num,i,j,strs);
+                        break;
+                    case 1, 2, 3:
+                        if (num < Integer.parseInt(strs[i - 1][j]) && num < Integer.parseInt(strs[i + 1][j]))
+                            offsetJ(num, i,j,strs);
+                    case 4:
+                        if (num < Integer.parseInt(strs[i - 1][j])) offsetJ(num,i,j,strs);
+        }
+        return strs[i][j];
+        }
+
+        static String offsetJ(int num, int i,int j, String[][]strs ){
+            switch (j){
+                case 0 : if(num<Integer.parseInt(strs[i][j+1])) return "*";
+                    break;
+                case 1,2,3: if(num<Integer.parseInt(strs[i][j-1]) && num<Integer.parseInt(strs[i][j+1])) return "*";
+                case  4 : if(num<Integer.parseInt(strs[i][j-1])) return "*";
+                    break;
+            }
+            return strs[i][j];
+        }
+    }
+
