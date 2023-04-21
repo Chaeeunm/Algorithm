51cfe6857a0e331a92ad3f17905bb67ab25a831f
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2023-04-21 09:44:00 +0900

    [Bronze III] Title: ACM 호텔, Time: 140 ms, Memory: 14520 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < tc; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            //input[0]호텔의 층 수, input[1]각 층의 방 수, input[3]몇 번째 손님
+            /*arr[2]%arr[0] = 손님이 묵을 층 수가 됨. 나머지가 0이면 층수 = arr[0]*/
+            /*손님이 묵을 방 번호 수 -> (arr[2]/arr[0])+1. 나머지가 0이면 arr[2]/arr[0]이 호수가 됨*/
+            int floor;
+            int lastNum;
+            if(input[2]%input[0]==0) {
+                floor=input[0];
+                lastNum = input[2]/input[0];
+            }
+            else {
+                floor = input[2] % input[0];
+                lastNum = input[2] / input[0] + 1;
+            }
+            /* 손님이 묵을 호수 = 층수 *100 + 방번호*/
+            sb.append(floor *100 + lastNum).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
