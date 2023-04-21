35a66a6359ef0b849c36161b6437f5bd310d2b40
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2023-04-21 09:26:13 +0900

    [Bronze II] Title: 분해합, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,27 +1,27 @@
+//분해합_2번째 방법
 
 import java.util.Scanner;
-
-//분해합 -1번째 방법
+//분해합 2번째 방법
 public class Main {
+    //N-N의 자릿수*9보다 작은 수는 생성자가 될 수 없다.
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int result =0;
+        String N = in.nextLine();
+        int result = 0;
+        int length = N.length();
+        int intN = Integer.parseInt(N);
 
-        for(int i = 0; i < N; i++) {
-            int number = i;
-            int sum = 0;	// 각 자릿수 합 변수
-
-            while(number != 0) {
-                sum += number % 10;	// 각 자릿수 더하기
-                number /= 10;
-            }
-
-            // i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우)
-            if(sum + i == N) {
-                result = i;
-                break;
-            }
+        for(int i = intN - (length*9); i<intN; i++){
+           int j = i;
+           int sum =0;
+           while(j !=0){
+               sum += j%10; //1의자리수를 더해줌
+               j /= 10; //1의자리수를 없애줌
+           }
+           if(sum+i==intN){
+               result = i;
+               break;
+           }
         }
         System.out.println(result);
     }
