fa914e9db53f938bec24b03a45c39f07be4ef1f0
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2023-05-01 02:30:18 +0900

    [Silver V] Title: 다리 놓기, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,32 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BigInteger[] dp = new BigInteger[31];
+    static int[][] dp = new int[31][31];
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        dp[1] = BigInteger.valueOf(1);
-        //dp[n] = n*dp[n-1]
-        while (tc-->0) {
+        while (tc-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
-            sb.append(bridge(n,m)).append("\n");
+            sb.append(bridge(n, m)).append("\n");
         }
         System.out.println(sb);
     }
-    static BigInteger bridge(int n, int m){
-        if(n==m) return BigInteger.valueOf(1);
-        return factorial(m).divide(factorial(n).multiply(factorial(m-n)));
-    }
-    static BigInteger factorial(int num){
-        if(dp[num]!=null) return dp[num];
-        return dp[num] = BigInteger.valueOf(num).multiply(factorial(num-1));
+
+    static int bridge(int n, int m) {
+        if (n == m || n == 0) return 1;
+        else if(dp[m][n]!=0) return dp[m][n];
+        return dp[m][n] = bridge(n - 1, m - 1) + bridge(n, m - 1);
     }
 }
