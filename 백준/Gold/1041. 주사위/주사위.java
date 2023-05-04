d144c441e2ad7c66a9abf2b305ee78e15bcda356
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2023-05-04 18:18:53 +0900

    [Gold V] Title: 주사위, Time: 124 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Gold/1041. 주사위/주사위.java
+++ 백준/Gold/1041. 주사위/주사위.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -26,12 +25,11 @@ public class Main {
 
         //A=0 B=1 C=2 D=3 E=4 F=5
         //CD,AF,EB는 안됨
-        for (int j = 0; j < 5; j++) {
-            for (int k = j + 1; k < 6; k++) {
-                if ((j == 0 && k == 5) || (j == 1 && k == 4) || (j == 2 && k == 3)) {
+        for (int j = 0; j < 6; j++) {
+            for (int k = 1; k < 6; k++) {
+                if (j == k || (j == 2 && k == 3) || (k == 2 && j == 3) || (k == 0 && j == 5) ||(k == 5 && j == 0) || (j == 4 && k == 1) || (k == 4 && j == 1))
                     continue;
-                }
-                m2 = Math.min(m2, arr[j] + arr[k]);
+                else m2 = Math.min(m2, arr[j] + arr[k]);
             }
         }
         //마주보는 면끼리는 같이 할 수 없음을 이용
@@ -45,15 +43,11 @@ public class Main {
         System.out.println(dice(N));
 
     }
-//1개 면 보일때 ->1 //2개 면 보일떄 -> 3 //3개 면 보일떄 ->6
-//3개 면 보이는 주사위: 무조건 4개
-// 2개 면 보이는 주사위 (n-1)*4+(n-2)*4 = 4(2n-3) = 8n-12개
-// 1개 면 보이는 주사위: 4(n-2)(n-1)+(n-2)^2 = 5n^2 - 16n + 12
     static long dice(long N) {
         long result3 = 4 * m3;
         long result2 = (8 * N - 12) * m2;
         long result1 = (5 * N * N - 16 * N + 12) * m1;
-        long result = N==1?arr[0]+arr[1]+arr[2]+arr[3]+arr[4]:result1+ result2 + result3;
+        long result = N==1?arr[0]+arr[1]+arr[2]+arr[3]+arr[4]:result1+ result2 + result3;       
         return result;
     }
 
