1aedab603c9e594c5c7f255c33c43d4669a6f76f
Chaeeunm <rlacodms0525@naver.com>
2023-05-30 23:59:28 +0900

    백준

---
+++ BaekJoon/Class3/Baek_5525.java
@@ -0,0 +1,24 @@
+package BaekJoon.Class3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_5525 {
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        //N+1개의 I와 N개의 O
+        //I와 O이 교대로 나오는 문자열
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        String S = br.readLine();
+
+
+    }
+    static void check(String s,int n){
+
+    }
+}
