503eec3d76ad49253bd84225aca381a0d909b239
Chaeeunm <rlacodms0525@naver.com>
2023-05-31 00:29:24 +0900

    백준 5525

--- BaekJoon/Class3/Baek_5525.java
+++ BaekJoon/Class3/Baek_5525.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baek_5525 {
-    static int count = 0;
     public static void main(String[] args) throws IOException {
         //N+1개의 I와 N개의 O
         //I와 O이 교대로 나오는 문자열
@@ -16,9 +15,20 @@ public class Baek_5525 {
 
         String S = br.readLine();
 
+        int count =0;
 
-    }
-    static void check(String s,int n){
-
-    }
+        for(int i=0; i<M; i++) {
+            if (S.charAt(i) == 'I') { //만약 i번째 수가 I면 다음수가 O
+                for(int j=i+1; j<2*N+i+1; j+=2){
+                    if(S.charAt(j)!='O'||S.charAt(j+1)!='I'){
+                        break;
+                    }
+                    if(j==2*N+i-1) count++;
+                }
+            }
+            //스트링을 돌며 I로 시작하는 경우를 찾음
+            //OI가 연속으로 N만큼 들어있는지 확인
+        }
+        System.out.println(count);
+        }
 }
