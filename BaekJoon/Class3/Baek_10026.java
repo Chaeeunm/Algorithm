7062c08dfe3820ccd259c03c36578fb2b920ae8b
Chaeeunm <rlacodms0525@naver.com>
2023-06-07 20:16:33 +0900

    백준 7576

--- BaekJoon/Class3/Baek_10026.java
+++ BaekJoon/Class3/Baek_10026.java
@@ -1,7 +1,5 @@
 package BaekJoon.Class3;
 
-import javafx.scene.chart.BubbleChart;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,15 +8,16 @@ import java.io.InputStreamReader;
 public class Baek_10026 {
 
     static int[] dx = {-1,0,0,1};
-    static int[] dy = {-1,0,0,1};
+    static int[] dy = {0,-1,1,0};
     static char[][] drawing;
     static boolean[][] visited;
-    static int cnt = 0;
-    static int cntWeakness = 0;
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        int cnt =0;
+        int WNCnt =0;
 
         visited = new boolean[N+1][N+1];
         drawing = new char[N+1][N+1];
@@ -29,11 +28,47 @@ public class Baek_10026 {
                 drawing[i][j] = s.charAt(j);
             }
         }
+
+        for(int i=0; i< N; i++){
+            for(int j=0; j<N; j++){
+                if(!visited[i][j]){
+                    dfs(i,j);
+                    cnt++;
+                }
+            }
+        }
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                if(drawing[i][j]=='G'){
+                    drawing[i][j] ='R';
+                }
+            }
+        }
+        visited = new boolean[N+1][N+1];
+        for(int i=0; i< N; i++){
+            for(int j=0; j<N; j++){
+                if(!visited[i][j]){
+                    dfs(i,j);
+                    WNCnt++;
+                }
+            }
+        }
+
+
+        System.out.println(cnt+" "+WNCnt);
     }
     static void dfs(int x, int y){
         visited[x][y] = true;
         char tmp = drawing[x][y];
-
+        for(int i=0; i<4; i++){
+            int a = x+ dx[i];
+            int b = y+ dy[i];
+            if(a<0||b<0||a>N||b>N){//테두리를 벗어나면 continue
+                continue;
+            }
+            if(!visited[a][b]&&drawing[a][b]==tmp) { //아직 방문하지 않았고,tmp와 같은 색이려면
+                dfs(a, b);
+            }
+        }
     }
-
 }
--- BaekJoon/Class3/Baek_7576.java
+++ BaekJoon/Class3/Baek_7576.java
@@ -4,13 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Baek_7576 {
     static int[][] box;
-    static Queue<Integer> tomato = new LinkedList<>();
+    static boolean[][] visited;
     static int cnt;
+    static int[] dx={-1,0,0,1};
+    static int[] dy ={0,-1,1,0};
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,14 +20,22 @@ public class Baek_7576 {
         int M = Integer.parseInt(br.readLine());
 
         box = new int[N][M];
+        visited = new boolean[N][M];
 
+        //1 익토 / 0안익토 / -1없토
         for (int i = 0; i < N; i++) {
             box[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
+        }
+        for(int i=0; i< N; i++){
+            for(int j=0; j<M; j++){
+                if(box[N][M]==1) { //토마토가 익어있으면 주변 토마토도 익게 하고, count++;
+                    dfs(j, i);
+                }
+            }
         }
     }
     //모든 구역을 탐색(모두 익을 때까지)
-    static void bfs(int start){
+    static void dfs(int x, int y){
 
     }
     //상하좌우를 넣어줌..?
