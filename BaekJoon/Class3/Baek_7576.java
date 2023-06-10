2e1e38065842500d92b2509b53594145587ae9b4
Chaeeunm <rlacodms0525@naver.com>
2023-06-10 23:58:27 +0900

    백준 7576

--- BaekJoon/Class3/Baek_7576.java
+++ BaekJoon/Class3/Baek_7576.java
@@ -14,6 +14,8 @@ public class Baek_7576 {
     static int[] dy = {0, -1, 1, 0};
     static int N;
     static int M;
+    static Queue<int[]> q = new LinkedList<>();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,15 +33,21 @@ public class Baek_7576 {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (box[N][M] == 1) { //토마토가 익어있으면 주변 토마토도 익게 하고, count++;
-                    change(j, i);
+                    q.add(new int[]{i,j});
                 }
             }
         }
-        cnt++;
+        bfs();
+        System.out.println();
     }
 
     static void bfs() {
-
+        while (!q.isEmpty()){
+            int[] t =q.poll();
+            int x = t[0];
+            int y = t[1];
+            change(x,y);
+        }
     }
 
     static boolean checkTomato() {
@@ -53,15 +61,16 @@ public class Baek_7576 {
         return false;
     }
 
-    //모든 구역을 탐색(모두 익을 때까지)
-    static void change(int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int a = x + dx[i];
-            int b = y + dy[i];
-            if (a < 0 || b < 0 || a > N || b > M) {
-                continue;
+        //모든 구역을 탐색(모두 익을 때까지)
+        static void change(int x, int y) {
+            for (int i = 0; i < 4; i++) {
+                int a = x + dx[i];
+                int b = y + dy[i];
+                if (a < 0 || b < 0 || a > N || b > M) {
+                    continue;
+                }
+                if (box[a][b] == 0) box[a][b] = 1;
+                q.add(new int[]{a, b});
             }
-            if (box[a][b] == 0) box[a][b] = 1;
         }
     }
-}
