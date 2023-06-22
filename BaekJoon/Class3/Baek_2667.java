82f0e24ddbae19395ebc91b77c097b7952e12421
Chaeeunm <rlacodms0525@naver.com>
2023-06-23 00:34:46 +0900

    백준 2667

--- BaekJoon/Class3/Baek_2667.java
+++ BaekJoon/Class3/Baek_2667.java
@@ -3,10 +3,18 @@ package BaekJoon.Class3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Baek_2667 {
 
-    static int[][] danji;
+    static int[][] dj;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static Queue<int[]> q = new LinkedList();
+    static boolean[][] visited;
+    static int N;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,15 +23,47 @@ public class Baek_2667 {
         //연결 => 집이 좌우, 상하로 다른 집이 있는 경우! (대각선 X)
         //각 단지에 속하는 집의 수를 오름차순 정렬
 
-        int N = Integer.parseInt(br.readLine());
-        danji = new int[N][N];
-        for(int i=0; i<N;i++){
+        N = Integer.parseInt(br.readLine());
+        dj = new int[N][N];
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
             String a = br.readLine();
-            for(int j=0; j<0; j++) {
-                danji[i][j] = Integer.parseInt(String.valueOf(a.charAt(j)));
+            for (int j = 0; j < N; j++) {
+                dj[i][j] = Integer.parseInt(String.valueOf(a.charAt(j)));
             }
         }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j] && dj[i][j] == 1) {
+                    visited[i][j]=true;
+                    q.add(new int[]{i, j});
+                    sb.append(bfs()).append('\n');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
 
-
+    static int bfs() {
+        int count = 0;
+        while (!q.isEmpty()) {
+            int[] t = q.poll();
+            count++;//q에서 하나 꺼낼때마다 count++
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    int y = t[0] + dy[i];
+                    int x = t[1] + dx[j];
+                    if(y<0||x<0||y>=N||x>=N){
+                        continue;
+                    }
+                    if (!visited[y][x] && dj[y][x] == 1) {//방문 안했고, 집이 있으면
+                        visited[y][x] =true;
+                        q.add(new int[]{y, x});
+                    }
+                }
+            }
+        }
+        return count;
     }
 }
