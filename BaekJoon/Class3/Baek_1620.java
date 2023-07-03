150f0030be9db81b5a3834ae1cd08021dc5aa7aa
Chaeeunm <rlacodms0525@naver.com>
2023-07-03 23:57:44 +0900

    백준 1620

--- BaekJoon/Class3/Baek_1620.java
+++ BaekJoon/Class3/Baek_1620.java
@@ -11,16 +11,25 @@ public class Baek_1620 {
 
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
+        int M= Integer.parseInt(st.nextToken());
 
-        HashMap<Integer,String> pocketmon = new HashMap<>(M); // HashMap은 저장공간보다 값이 추가로 들어오면 약 두배로 늘립니다. 과부하 발생할 수 있기에 초기 저장공간 설정해주는게좋음
+        HashMap<Integer,String> pocketmonI = new HashMap<>(N); // HashMap은 저장공간보다 값이 추가로 들어오면 약 두배로 늘립니다. 과부하 발생할 수 있기에 초기 저장공간 설정해주는게좋음
+        HashMap<String,Integer> pocketmonS = new HashMap<>(N);
 
-        for (int i=0;i<M; i++){
-            pocketmon.put(i,br.readLine());
+        for (int i=1;i<=N; i++){
+            String pock = br.readLine();
+            pocketmonI.put(i,pock);
+            pocketmonS.put(pock,i);
         }
-        while(N-->0){
-
+        StringBuilder sb = new StringBuilder();
+        while(M-->0){
+            String s = br.readLine();
+            if(Character.isDigit(s.charAt(0))){//숫자이면
+                sb.append(pocketmonI.get(Integer.parseInt(s))).append('\n');
+            }
+            else sb.append(pocketmonS.get(s)).append('\n');
         }
+        System.out.println(sb);
     }
 }
