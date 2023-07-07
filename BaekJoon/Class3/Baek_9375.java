a9754054cd385a5a2df3cbbedf5bd8f39fbbfcaa
Chaeeunm <rlacodms0525@naver.com>
2023-07-07 23:23:58 +0900

    백준 9375

---
+++ BaekJoon/Class3/Baek_9375.java
@@ -0,0 +1,15 @@
+package BaekJoon.Class3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_9375 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
