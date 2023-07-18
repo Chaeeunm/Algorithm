7c23fc4dae70e617e3515296eb33652a31f9adca
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2023-07-18 14:34:51 +0900

    [Silver I] Title: 경로 찾기, Time: 188 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//가중치 없는 방향 그래프 G가 주어졌을 때, 모든 정점 (i, j)에 대해서, i에서 j로 가는 길이가 양수인 경로가 있는지 없는지 구하는 프로그램을 작성하시오.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] graph = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) graph[i][j] = 1;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(graph[i][j]).append(" ");
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+}
