192ddef7e76aa677f6e454986fb5c4138f11f740
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2024-01-14 01:10:31 +0900

    [Bronze II] Title: 시그마, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/2355. 시그마/시그마.java
+++ 백준/Bronze/2355. 시그마/시그마.java
@@ -5,27 +5,28 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        long max = Math.max(a, b);
-        long min = Math.min(a, b);
+        long max = Math.max(a,b);
+        long min = Math.min(a,b);
 
-        long num = (max - min + 1);
+        long num = (max - min +1);
         long result;
 
-        if (a == b) {
+        if(a == b ){
             result = a;
-        } else if (num % 2 == 0) {
-            result = (max + min) * (num / 2);
-        } else {
+        }
+        else if(num %2 == 0 ){
+            result = (max+min) * (num/2);
+        }
+        else {
             result = (max + min) * (num / 2) + (max + min) / 2;
         }
-
         System.out.println(result);
     }
 }
