24abff64d4c5a8d514717b483f9bfd313baba4ec
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2024-01-22 00:58:02 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,34 +1,42 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st =  new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
+
         int M = Integer.parseInt(st.nextToken());
-        String s = br.readLine();
-        String input[] = s.split(" ");
-        /*stream을 통해 int 배열로 바꿔줌*/
-        int[] cardNum = Arrays.stream(input).mapToInt(Integer::parseInt).sorted().toArray();
 
-        int result=0;
-        for (int i = 0; i < cardNum.length - 2; i++) {
+        int[] cards= new int[N];
 
-            for (int j = i + 1; j < cardNum.length - 1; j++) {
+        st = new StringTokenizer(br.readLine(), " ");
+        //3개 골라서 M과의 차가 더 작으면 저장
+        for(int i=0; i<N; i++){
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
 
-                for (int k = j + 1; k < cardNum.length; k++) {
-                    int sum = cardNum[i] + cardNum[j] + cardNum[k];
+        int result = -1;
 
-                    /*합이 M을 넘지 않으면서 가장 큰 경우*/
-                    if (result < sum && sum <= M) {
-                        result = sum;
-                    }
+        for(int i =0; i<N-2; i++) {
+            int f = cards[i];
+            for(int j = i+1 ; j<N-1; j++){
+                int s = cards[j];
+                for(int k = j+1; k<N; k++ ) {
+                int t = cards[k];
+                if(result == -1){
+                    if(M>= (f+s+t))
+                    result = f+s+t;
+                }
+                if(M>= (f+s+t) && M-result > M-(f+s+t)){
+                    result = f+s+t;
+                }
                 }
             }
         }
