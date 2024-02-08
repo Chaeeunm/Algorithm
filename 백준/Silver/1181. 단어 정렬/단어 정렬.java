0b27cd186e726e34123a76259813c9ba694d4fe9
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2024-02-09 02:04:31 +0900

    [Silver V] Title: 단어 정렬, Time: 376 ms, Memory: 29236 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+        for(int i=0; i<N; i++){
+            arr[i]=br.readLine();
+        }
+//길이가 짧은 것부터, 길이가 같으면 사전 순으로 , 중복제거
+
+        Arrays.sort(arr,new Comparator<String>(){
+
+            @Override
+            public int compare(String str1,String str2){
+                if(str1.length()==str2.length()){ //비교대상 길이가 같으면 사전순 정렬
+                    return str1.compareTo(str2);
+                }
+                else return str1.length() - str2.length(); //다르면 길이가 짧은 것부터 (음수이면 자리바꿈)
+            }
+        });
+        String[] resultArr = Arrays.stream(arr).distinct().toArray(String[]::new);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<resultArr.length; i++){
+            sb.append(resultArr[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
