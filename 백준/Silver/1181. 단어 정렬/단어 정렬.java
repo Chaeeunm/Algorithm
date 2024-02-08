9719f7d2dc92de8527db81191e08fe0e00d66033
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2024-02-09 02:32:14 +0900

    [Silver V] Title: 단어 정렬, Time: 368 ms, Memory: 25716 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,35 +1,41 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
+//길이가 짧은 것부터
+//길이가 같으면 사전순으로
+//중복된 단어는 하나만 남기고 제거
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String[] arr = new String[N];
-        for(int i=0; i<N; i++){
-            arr[i]=br.readLine();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[n];
+
+        for(int i =0; i<n; i++) {
+            arr[i] = br.readLine();
         }
-//길이가 짧은 것부터, 길이가 같으면 사전 순으로 , 중복제거
 
-        Arrays.sort(arr,new Comparator<String>(){
-
-            @Override
-            public int compare(String str1,String str2){
-                if(str1.length()==str2.length()){ //비교대상 길이가 같으면 사전순 정렬
-                    return str1.compareTo(str2);
-                }
-                else return str1.length() - str2.length(); //다르면 길이가 짧은 것부터 (음수이면 자리바꿈)
-            }
-        });
-        String[] resultArr = Arrays.stream(arr).distinct().toArray(String[]::new);
-
+       Arrays.sort(arr, new Comparator<String>() {
+           @Override
+           public int compare(String s1, String s2) {
+               if(s1.length() == s2.length())  {
+                   return s1.compareTo(s2);
+               }
+               //그렇지 않으면 길이 오름차순대로
+               return s1.length() - s2.length();
+           }
+       });
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<resultArr.length; i++){
-            sb.append(resultArr[i]).append('\n');
-        }
+
+
+        Arrays.stream(arr).distinct().forEach(a -> sb.append(a).append('\n'));
+
         System.out.println(sb);
     }
 }
