68f853554ae3c6adb6472299650d76341f90c03a
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2024-02-09 09:41:31 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 704 ms, Memory: 50252 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] co = new int[n][2];
+
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            co[i][0] = Integer.parseInt(st.nextToken());
+            co[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(co, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                //와이좌표가 같으면 엑스좌표가 증가하는 순으로
+                if(o1[1] == o2[1]) return o1[0] - o2[0];
+                //그렇지 않으면 와이좌표가 증가하는 순으로
+                return o1[1] -o2[1];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(co).forEach(a -> sb.append(a[0]).append(" ").append(a[1]).append('\n'));
+
+        System.out.println(sb);
+    }
+}
+
