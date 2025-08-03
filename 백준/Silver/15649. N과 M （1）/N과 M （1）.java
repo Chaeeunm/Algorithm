d9b859c99942965f148740c17d5f7ba30a3c6305
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2025-08-03 23:14:32 +0900

    [Silver III] Title: N과 M (1), Time: 348 ms, Memory: 29836 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,44 +1,44 @@
-
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
-	static int n;
-	static int m;
-
-	static int[] output;
 	static boolean[] visited;
+	static List<Integer> selected = new ArrayList<>();
+	static StringBuilder sb;
 
-	public static void main (String[] args) throws IOException {
-
+	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 
-		n = sc.nextInt();
-		m = sc.nextInt();
+		visited = new boolean[n+1];
 
-		output = new int[m];
-		visited = new boolean[n + 1];
+		sb = new StringBuilder();
 
-		BT(0);
+		//n까지 돌면서 방문안했으면 depth +1해서 다시 dfs
+
+		dfs(n,m);
+		System.out.println(sb);
 	}
 
-	//숫자
-	public static void BT(int depth) {
-		if(depth == m) {
-			for(int num : output) {
-				System.out.print(num + " ");
+	public static void dfs(int n, int m){
+		if(m==0) {
+			for(int now: selected){
+				sb.append(now).append(" ");
 			}
-			System.out.println();
+			sb.append('\n');
 			return;
 		}
-		for(int i =1; i <= n ; i++) {
+		for(int i=1; i<=n; i++){
 			if(!visited[i]){
 				visited[i] = true;
-				output[depth] = i;
-				BT(depth +1);
+				selected.add(i);
+				dfs(n,m-1);
 				visited[i] = false;
+				selected.remove(selected.size()-1);
 			}
 		}
 	}
+
 }
