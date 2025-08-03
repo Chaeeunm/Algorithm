d6769ced6ca591411cc9f13421c44881a5bf13e0
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2025-08-03 21:57:12 +0900

    [Silver V] Title: 단어 정렬, Time: 296 ms, Memory: 20640 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -3,10 +3,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.TreeSet;
+
 
 public class Main {
 	//길이가 짧은것부타,
@@ -17,21 +16,21 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
-		Set<String> set = new HashSet<>();
+
+		//내부적으로 이진탐색 트리 사용 O(log N) 따라서 저장다한담에 정렬하는거랑 속도는 같음
+		Set<String> set = new TreeSet<>((a,b) -> {
+			if(a.length() == b.length()) return a.compareTo(b);
+			else return a.length() - b.length();
+		});
 
 		while (N-->0){
 			set.add(br.readLine());
 		}
 
-		List<String> sorted = set.stream().sorted((a,b) -> {
-			if( a.length() == b.length()){
-				return a.compareTo(b);
-			} else return a.length() - b.length();
-		}).collect(Collectors.toList());
 
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		for(String now : sorted){
+		for(String now : set){
 			bw.write(now);
 			bw.newLine();
 		}
