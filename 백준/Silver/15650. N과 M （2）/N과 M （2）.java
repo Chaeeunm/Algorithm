9b6be666e7898e012af1d088566c51d1c3a55c95
김채은 <111339014+Chaeeunm@users.noreply.github.com>
2025-08-03 23:36:24 +0900

    [Silver III] Title: N과 M (2), Time: 196 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	static boolean[] visited;
+	static List<Integer> selected = new ArrayList<>();
+	static StringBuilder sb;
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		visited = new boolean[n+1];
+
+		sb = new StringBuilder();
+
+		dfs(1,n,m);
+		System.out.println(sb);
+	}
+
+	public static void dfs(int start, int n, int m){
+		if(m==0) {
+			for(int now: selected){
+				sb.append(now).append(" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		for(int i = start; i<=n; i++){
+			if(!visited[i]){
+				visited[i] = true;
+				selected.add(i);
+				dfs(i+1, n,m-1);
+				visited[i] = false;
+				selected.remove(selected.size()-1);
+			}
+		}
+	}
+
+}
